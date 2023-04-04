afc29f7a729e85025b463e28b3d3d0bfae8aeae9
AgusLacomi <aguslacomi@gmail.com>
2023-04-04 20:15:31 -0300

    Mejoras realizadas en punto 2

--- ejercicios/diferenciaSimetrica.go
+++ ejercicios/diferenciaSimetrica.go
@@ -3,7 +3,16 @@ package ejercicios
 import (
 	"guia4/set"
 )
-
+// By AgusLacomi Punto 3
 func DiferenciaSimetrica[T comparable](s1, s2 *set.Set[T]) *set.Set[T] {
-	panic("Not implemented")
+
+	s3 := set.NewSet[T]()
+
+	for _, elemento := range s1.Values() {
+		if !s2.Contains(elemento) {
+			s3.Add(elemento)
+		}
+	}
+
+	return s3
 }
--- ejercicios/eliminarRepetidos.go
+++ ejercicios/eliminarRepetidos.go
@@ -5,11 +5,7 @@ import "guia4/set"
 //By AgusLacomi Punto 4
 func EliminarRepetidos[T comparable](arreglo []T) []T {
 
-	conjunto := set.NewSet[T]()
-
-	for _, elemento := range arreglo {
-		conjunto.Add(elemento)
-	}
+	conjunto := set.NewSet(arreglo...)
 
 	return conjunto.Values()
 }
